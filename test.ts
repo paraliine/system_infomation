515a1b4d8a1a9467e46e950733b0a3eeafb43b84
Peng Xiaoxuan <116947561+paraliine@users.noreply.github.com>
2025-07-30 15:46:13 +0800

    ci: test GitHub pull request workflow
    Changed html structure

--- test.ts
+++ test.ts
@@ -1,7 +1,13 @@
 export class kkkzoz{
     name: string;
-    constructor(name:string) {
+    age: number;
+    
+    constructor(name: string, age: number) {
         this.name = name;
+        this.age = age;
+    }
+    greet(): string {
+        return `Hello, my name is ${this.name}!`;
     }
 }
 
